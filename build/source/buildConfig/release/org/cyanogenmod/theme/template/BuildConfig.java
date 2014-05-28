f91c65eb9c7dbc49b2baf014ccbfa4e4461fb58e
neatchee <neatchee@gmail.com>
2014-05-27 18:23:18 -0700

    Initial upload
    Change-Id: I1a70a73d8cd77a5a314ce6386fceebcfd0d58b34

---
+++ build/source/buildConfig/release/org/cyanogenmod/theme/template/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.cyanogenmod.theme.template;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "org.cyanogenmod.theme.template";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
